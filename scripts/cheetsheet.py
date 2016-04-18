0e4dbd4e61f4ffa43fc891cbc9874a6b9f04b75b
fsociety <dividebyzero@teknik.io>
2016-04-18 14:20:43 +1000

    added small python script to remember cmdline utils installed

---
+++ scripts/cheetsheet.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+from colorama import Fore
+
+print(' ')
+print(Fore.GREEN + '::      CMDLine Cheat Sheet      ::')
+print(Fore.YELLOW + "::   Name          Description   ::")
+print(' ')
+print(Fore.WHITE + "     ghi           Github Issue Utility")
+print(Fore.WHITE + "     trelew        Trellos Msg Board Utility")
+print(' ')
+
+
